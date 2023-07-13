0ae9fedfd88d4f7835a88ee32a6dd8c86c884eba
Пользователь <suhrob.coder0604@gmail.com>
2023-07-13 15:53:23 +0300

    dsgsdf

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -12,21 +12,15 @@ import { AboutWrapper } from "./AboutWrapper";
 export default function About() {
   // Data
   const data = useContext(Data);
-  const { title, description, question, answer, resumeLink, img } =
-    data.aboutMe;
-    
+  const { title, description, question, answer, resumeLink } = data.aboutMe;
+
   return (
     <AboutWrapper>
       <h1>
         <Fade>About Me</Fade>
       </h1>
       <Row>
-        <Col className="img-wrapper" lg={4} md={6} sm={6}>
-          <Slide>
-            <img className="img mx-auto" src={img} />
-          </Slide>
-        </Col>
-        <Col className="pt-5 mb-2" md={6} lg={5} sm={6}>
+        <Col className="pt-3 mb-2" md={6} lg={5} sm={6}>
           <Slide direction="right">
             <h5>{title}</h5>
             <h6>{description}</h6>
